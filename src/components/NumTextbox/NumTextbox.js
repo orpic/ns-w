70ba16eed4450718d68f5ef26c7d2f7ae2de2418
shobhit-u <shobhitumrao5@gmail.com>
2023-01-03 00:39:46 +0530

    text box divided into two

--- src/components/NumTextbox/NumTextbox.js
+++ src/components/NumTextbox/NumTextbox.js
@@ -11,8 +11,12 @@ const NumTextbox = () => {
         ( WhatsApp <span>पे मैसेज भेजे बिना नंबर सेव किए</span> )
       </p>
       <label className="numTextbox-enter-number">
-        Enter Number <span>(नीचे दिए स्थान पर नंबर डालें)</span>
+        Enter Number <span>( नीचे दिए स्थान पर नंबर डालें )</span>
       </label>
+      <div className="numTextbox-inputs">
+        <input className="numTextbox-input input-country-code" type="number" />
+        <input className="numTextbox-input input-mobile-num" type="number" />
+      </div>
     </div>
   );
 };
