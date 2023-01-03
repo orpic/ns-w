5fa9b07069939a6270a55af17b5a6f754353d793
shobhit-u <shobhitumrao5@gmail.com>
2023-01-03 14:59:39 +0530

    basic ui complete

--- src/components/ChatButton/ChatButton.js
+++ src/components/ChatButton/ChatButton.js
@@ -5,7 +5,7 @@ const ChatButton = () => {
   return (
     <div className="gen-col-container">
       <button className="numTextbox-btn">
-        <p className="chatButton-text">Open chat screen</p>
+        <p className="chatButton-text gen-bold-text">Open chat screen</p>
         <p className="chatButton-text">( चैट स्क्रीन पे जाएं )</p>
       </button>
     </div>
--- src/components/NumTextbox/NumTextbox.js
+++ src/components/NumTextbox/NumTextbox.js
@@ -10,12 +10,32 @@ const NumTextbox = () => {
       <p className="numTextbox-heading">
         ( WhatsApp <span>पे मैसेज भेजे बिना नंबर सेव किए</span> )
       </p>
-      <label className="numTextbox-enter-number">
+      <label id="mob-num" className="numTextbox-enter-number">
         Enter Number <span>( नीचे दिए स्थान पर नंबर डालें )</span>
       </label>
       <div className="numTextbox-inputs">
-        <input className="numTextbox-input input-country-code" type="number" />
-        <input className="numTextbox-input input-mobile-num" type="number" />
+        <button className="numTextbox-input input-country-code">+91</button>
+        <input
+          id="mob-num"
+          className="numTextbox-input input-mobile-num"
+          type="number"
+        />
+        <button className="numTextbox-input clear-input-mobile-num gen-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
