f3a7f62016e34d8289abc51069f5f3ac0b03f9d8
shobhit-u <shobhitumrao5@gmail.com>
2023-01-03 15:53:27 +0530

    numTextbox input && clear handler complete

--- src/components/NumTextbox/NumTextbox.js
+++ src/components/NumTextbox/NumTextbox.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NumTextbox.css";
 
 const NumTextbox = () => {
+  const [numInput, setNumInput] = useState("");
+
+  const onNumInputChangeHandler = (event) => {
+    const limit = 10;
+    const val = event.target.value.slice(0, limit);
+    setNumInput(val);
+    // console.log(val);
+  };
+
+  const onNumInputClearHandler = () => {
+    setNumInput("");
+  };
+
   return (
     <div className="gen-col-container">
       <h1 className="numTextbox-heading">
@@ -10,28 +23,33 @@ const NumTextbox = () => {
       <p className="numTextbox-heading">
         ( WhatsApp <span>पे मैसेज भेजे बिना नंबर सेव किए</span> )
       </p>
-      <label id="mob-num" className="numTextbox-enter-number">
+      <label className="numTextbox-enter-number">
         Enter Number <span>( नीचे दिए स्थान पर नंबर डालें )</span>
       </label>
       <div className="numTextbox-inputs">
         <button className="numTextbox-input input-country-code">+91</button>
         <input
-          id="mob-num"
+          value={numInput}
+          onChange={onNumInputChangeHandler}
           className="numTextbox-input input-mobile-num"
           type="number"
+          maxLength="10"
         />
-        <button className="numTextbox-input clear-input-mobile-num gen-center">
+        <button
+          onClick={onNumInputClearHandler}
+          className="numTextbox-input clear-input-mobile-num gen-center"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
-            class="w-6 h-6"
+            className="w-6 h-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M6 18L18 6M6 6l12 12"
             />
           </svg>
